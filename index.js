050f750afc2b5cf7c791d159de1ece96bb034312
yassinechamli <52163864+yassinechamli@users.noreply.github.com>
2019-08-26 17:53:59 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const pets = [
+    { name: "Max", type: "dog", bornOn: 2018 },
+    { name: "Angel", type: "cat", bornOn: 2015 },
+    { name: "Jasper", type: "dog", bornOn: 2016 }
+  ];
+  
+  const getAge= (pet) => {
+    return new Date().getFullYear() - pet.bornOn;
+  }
+  
+  let petsWithAge = [];
+  pets.forEach( (pet,i) => {
+    pet = pets[i];
+    pet.age = getAge(pet);
+  
+    petsWithAge.push(pet);
+  }
+  )
+  console.log(petsWithAge)
+  
+  let dogs = [];
+  pets.find( (pet,i) => {
+     pet = pets[i];
+    if (pet.type === "dog") {
+      dogs.push(pet);
+  } 
+    
+    })
+  
+  console.log(dogs);
+  
+  let jasper;
+  pets.forEach((pet,i)=> {
+     pet = pets[i];
+    if (pet.name === "Jasper") {
+      jasper = pet;
+    }
+  })
+  console.log(`Jasper is  ${jasper.age}  years old`);
